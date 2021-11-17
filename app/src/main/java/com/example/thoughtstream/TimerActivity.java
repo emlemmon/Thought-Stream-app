f5884deab3795b5324441c628707a660221d3bcb
Dylon Jaynes <dylonjaynes@gmail.com>
2021-11-16 18:54:22 -0700

    Fixing merge conflicts.

--- app/src/main/java/com/example/thoughtstream/TimerActivity.java
+++ app/src/main/java/com/example/thoughtstream/TimerActivity.java
@@ -1,126 +1,31 @@
-<<<<<<< HEAD
 package com.example.thoughtstream;
-
-import android.animation.ObjectAnimator;
-<<<<<<<< HEAD:app/src/main/java/com/example/thoughtstream/TimerActivity.java
-import android.os.Bundle;
-import android.os.Handler;
-========
-import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
-import android.os.Handler;
-import android.os.Bundle;
-import android.view.View;
->>>>>>>> a9468863f67759a1629c2e1b00a04ff7a170ea59:app/src/main/java/com/example/progressbarpractice/MainActivity.java
-import android.view.animation.DecelerateInterpolator;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.thoughtstream.MainActivity;
-
-public class TimerActivity extends AppCompatActivity {
-
-    private final Handler handler = new Handler();
-
-    TextView tv;
-
-<<<<<<<< HEAD:app/src/main/java/com/example/thoughtstream/TimerActivity.java
-========
-    private Button TESTINGBUTTON;
-
->>>>>>>> a9468863f67759a1629c2e1b00a04ff7a170ea59:app/src/main/java/com/example/progressbarpractice/MainActivity.java
-=======
-package com.example.progressbarpractice;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
+import androidx.appcompat.app.AppCompatActivity;
 import java.util.Locale;
 
 public class TimerActivity extends AppCompatActivity {
     private static final long START_TIME_IN_MILLIS = 600000;
-
     private TextView mTextViewCountDown;
     private Button mButtonStart;
     private Button mButtonPause;
     private Button mButtonReset;
-
     private CountDownTimer mCountDownTimer;
-
     private boolean mTimerRunning;
-
     private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
 
->>>>>>> a9468863f67759a1629c2e1b00a04ff7a170ea59
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
-<<<<<<< HEAD
-        final ProgressBar mProgress = (ProgressBar) findViewById(R.id.circularProgressbar);
-        mProgress.setProgress(0);   // Main Progress
-        mProgress.setSecondaryProgress(100); // Secondary Progress
-        mProgress.setMax(100); // Maximum Progress
-        ObjectAnimator animation = ObjectAnimator.ofInt(mProgress, "progress", 0, 100);
-        animation.setDuration(50000);
-        animation.setInterpolator(new DecelerateInterpolator());
-        animation.start();
-        tv = (TextView) findViewById(R.id.tv);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                // TODO Auto-generated method stub
-                while (mProgress.getProgress() <= 100) {
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            // TODO Auto-generated method stub
-                            tv.setText(mProgress.getProgress() + "%");
-                        }
-                    });
-                    try {
-                        // Sleep for 200 milliseconds.
-                        // Just to display the progress slowly
-                        Thread.sleep(16); //thread will take approx 3 seconds to finish
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
-
-        TESTINGBUTTON = findViewById(R.id.button_send);
-        TESTINGBUTTON.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goTotimer(v);
-            }
-        });
-
-    }
-
-    public void goTotimer (View view) {
-        Intent intent = new Intent(this, TimerActivity.class);
-        startActivity(intent);
-    }
-}
-=======
-
         mTextViewCountDown = findViewById(R.id.text_view_countdown);
-
         mButtonStart = findViewById(R.id.button_start);
         mButtonPause = findViewById(R.id.button_pause);
         mButtonReset = findViewById(R.id.button_reset);
-
         mButtonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -159,7 +64,6 @@ public class TimerActivity extends AppCompatActivity {
                 mButtonReset.setVisibility(View.VISIBLE);
             }
         }.start();
-
         mTimerRunning = true;
         mButtonReset.setVisibility(View.INVISIBLE);
     }
@@ -180,13 +84,11 @@ public class TimerActivity extends AppCompatActivity {
     private void updateCountDownText() {
         int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
         int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
-
         String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
-
         mTextViewCountDown.setText(timeLeftFormatted);
     }
 }
->>>>>>> a9468863f67759a1629c2e1b00a04ff7a170ea59
+
 
 //        package com.example.thoughtstream;
 //
