4f457cca51b18a5eb52357346b771ca72cd0b69e
Dylon Jaynes <dylonjaynes@gmail.com>
2021-11-23 17:57:41 -0700

    Adding activity_thoughts.xml along with some navigation code.

--- app/src/main/java/com/example/thoughtstream/ThoughtsActivity.java
+++ app/src/main/java/com/example/thoughtstream/ThoughtsActivity.java
@@ -1,6 +1,28 @@
 package com.example.thoughtstream;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ThoughtsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thoughts);
+
+        ImageView plusImg = findViewById(R.id.imageViewAddButton);
+        plusImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ThoughtsActivity.this, NewThoughtActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
