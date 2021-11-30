6b149f7ffb074ed969017f3df5d100d77cb0a685
emlemmon <lem19012@byui.edu>
2021-11-29 20:40:47 -0700

    change buttons and backgrounds, font and sizes

--- app/src/main/java/com/example/thoughtstream/MainActivity.java
+++ app/src/main/java/com/example/thoughtstream/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar(findViewById(R.id.app_bar));
     }
 
     @Override
